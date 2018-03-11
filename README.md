c6f43fd0a9a70eafadb371c2e40570c08773b360
Beeblebrax <aadarsh.shetty@gmail.com>
2018-03-11 18:57:21 -0400

    Initial commit for insertion sort

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a simple C++ implementation of insertion sort algorithm for integer arrays.
---
+++ insertionsort.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int size;
+	cout << "Enter array size: " << '\n';
+	cin >> size;
+	int arr[size];
+	for (int i = 0; i<size;++i){
+		cin >> arr[i];
+	}
+	for (int j = 1; j < size; ++j){
+		for (int k =j; k>=0;--k){
+			if (arr[k] < arr[k -1]){
+				arr[k] += arr[k-1];
+				arr[k-1] = arr[k] - arr[k-1];
+				arr[k] = arr[k] - arr[k-1];
+			}
+			else
+				break;
+		}
+	}
+	cout << "Sorted array: " << '\n';
+	for (int i = 0;i<size;++i){
+		cout <<arr[i] << "    ";
+	}
+}
