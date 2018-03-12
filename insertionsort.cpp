0cfcbca5d7030b82660a81a0de72cccae6ecfc05
Beeblebrax <aadarsh.shetty@gmail.com>
2018-03-12 19:17:10 -0400

    Separatng inserion sort algo to a function

--- insertionsort.cpp
+++ insertionsort.cpp
@@ -1,14 +1,9 @@
 #include<iostream>
 using namespace std;
 
-int main(){
-	int size;
-	cout << "Enter array size: " << '\n';
-	cin >> size;
-	int arr[size];
-	for (int i = 0; i<size;++i){
-		cin >> arr[i];
-	}
+void insertionSort(int *arr, int n)
+{
+	int size = n;
 	for (int j = 1; j < size; ++j){
 		for (int k =j; k>=0;--k){
 			if (arr[k] < arr[k -1]){
@@ -20,6 +15,19 @@ int main(){
 				break;
 		}
 	}
+}
+
+int main(){
+	int size;
+	cout << "Enter array size: " << '\n';
+	cin >> size;
+	int arr[size];
+	for (int i = 0; i<size;++i){
+		cin >> arr[i];
+	}
+
+	insertionSort(arr, size);
+
 	cout << "Sorted array: " << '\n';
 	for (int i = 0;i<size;++i){
 		cout <<arr[i] << "    ";
